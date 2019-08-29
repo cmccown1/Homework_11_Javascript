8c0457e96a4f353286f79b0c66966755bc3a41cb
cmccown <cmccown1@gmail.com>
2019-08-29 17:04:25 -0400

    added more comments

--- static/js/app.js
+++ static/js/app.js
@@ -1,21 +1,18 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
-
-
 // select the button
 var button = d3.select("#filter-btn");
 
 button.on("click", function() {
 
-    // add more input fields to index.html
     // select the input element and get the raw html node
     var inputDate = d3.select("#datetime");
     var inputCity = d3.select("#city");
     var inputState = d3.select("#state");
     var inputCountry = d3.select("#country");
     var inputShape = d3.select("#shape");
+
     // get the value property of the input element
     var dateValue = inputDate.property("value");
     var cityValue = inputCity.property("value");
@@ -23,34 +20,36 @@ button.on("click", function() {
     var countryValue = inputCountry.property("value");
     var shapeValue = inputShape.property("value");
 
-    // filter the table data
-    // only apply filters when a filter value is not empty
+    // filter the table data; only apply filters when a filter value is not empty
 
+    // the default, w/no filter data, is to show the entire table of all sightings
     var filteredData = tableData;
 
+    // if there's a value for the date filter, apply it
     if (dateValue != "") {
         filteredData = filteredData.filter(sighting => sighting.datetime === dateValue);
     };
+    // if there's a value for the city filter, apply it
     if (cityValue != "") {
         filteredData = filteredData.filter(sighting => sighting.city === cityValue);
     };
+    // if there's a value for the state filter, apply it
     if (stateValue != "") {
         filteredData = filteredData.filter(sighting => sighting.state === stateValue);
     };
+    // if there's a value for the country filter, apply it
     if (countryValue != "") {
         filteredData = filteredData.filter(sighting => sighting.country === countryValue);
     };
+    // if there's a value for the shape filter, apply it
     if (shapeValue != "") {
         filteredData = filteredData.filter(sighting => sighting.shape === shapeValue);
-    };
-         
-    
-    
-
+    }; 
 
     // select the table body
     var tbody = d3.select("#ufo-table>tbody");
 
+    // clear the table
     tbody.html("");
 
     // iterate through each sighting object in the filteredData array
